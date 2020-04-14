b90bed533e036e1e6a9b053559fd3be9a928eecd
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-14 17:36:20 +0500

    setup: navbar and styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,43 @@
-import { Link } from "gatsby"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap"
+
+const Header = ({ siteTitle }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar fixed="top" light expand="sm">
+        <NavbarBrand href="/">{siteTitle}</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/team/">Team</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/tags/">Tags</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about/">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  </header>
-)
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "../styles/index.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,13 +26,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div className="container" id="content">
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
